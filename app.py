33201d9f4b63b9dde9089232977b4b91b1eb29f2
probe <re2rar@gmail.com>
2020-07-01 00:01:22 +0900

    sqlite 추가.

--- app.py
+++ app.py
@@ -1,12 +1,69 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime, timezone
+import logging
 
 app = Flask(__name__)
 
+# sqlite
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route('/')
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    # Auto increment
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.now(timezone.utc))
+
+    def __repr__(self):
+        return f'<Task {self.id}>'
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('index.html', name="probe")
+    if request.method == 'POST':
+        new_task = Todo(content=request.form['content'])
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            logging.exception(e)
+            return '디비에 추가 불가'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
 
 
-if __name__ == "__main__":
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        logging.exception(e)
+        return '디비 삭제 에러'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if (request.method == 'POST'):
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except Exception as e:
+            logging.exception(e)
+            return '디비 업데이트 에러'
+    else:
+        return render_template('update.html', task=task)
+
+
+if __name__ == '__main__':
     app.run(debug=True)
